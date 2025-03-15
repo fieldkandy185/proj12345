de2c66853985077ef2a2e5cdc820f81642d8ef45
fieldkandy185 <fieldkandy185@gmail.com>
2025-03-15 22:07:07 +0000

    Add 10.php

---
+++ src/10.php
@@ -0,0 +1,10 @@
+ <?php 
+// Random PHP Code Generator
+
+$randomNum = rand(1, 10);
+if ($randomNum <= 5) {
+    echo "The number is less than or equal to 5";
+} else {
+    echo "The number is greater than 5";
+}
+?>
