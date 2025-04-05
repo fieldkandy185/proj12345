f423355a7825fe211f78939118b1eedbd4c8876b
fieldkandy185 <fieldkandy185@gmail.com>
2025-04-05 06:27:38 +0000

    Add 25.php

---
+++ src/25.php
@@ -0,0 +1,5 @@
+<?php
+// This is a simple PHP script that randomly generates and outputs a number between 1 and 10.
+$random_number = rand(1, 10);
+echo "The random number generated is: $random_number";
+?>
